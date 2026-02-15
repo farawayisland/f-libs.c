1d6279abf70a5627282dd71afed732aba7d0f11c
farawayisland <207914612+farawayisland@users.noreply.github.com>
2026-02-16 06:07:10 +0700

    fix(flist): add explicit `extern` linkage specifiers and change `fmath.h` to `fstat.h`

--- include/flist.h
+++ include/flist.h
@@ -3,7 +3,7 @@
 #define FLIST_H_
 
 #include "fio.h"
-#include "fmath.h"
+#include "fstat.h"
 
 /* Singly-Linked List */
 // List-node data
@@ -29,71 +29,72 @@ struct SList {
 };
 
 // Memory allocations and deallocations for list nodes and lists
-struct SList *init_empty_list_s(void);
-struct SList *init_list_s(const char *id, int val);
-struct SListNode *init_empty_node_s(void);
-struct SListNode *init_node_s(const char *id, int val);
-void free_list_s(struct SList **list_ref);
-void free_node_s(struct SListNode **node_ref);
+extern struct SList *init_empty_list_s(void);
+extern struct SList *init_list_s(const char *id, int val);
+extern struct SListNode *init_empty_node_s(void);
+extern struct SListNode *init_node_s(const char *id, int val);
+extern void free_list_s(struct SList **list_ref);
+extern void free_node_s(struct SListNode **node_ref);
 
 // Operations for list nodes and lists
 /// List-node data
-char *get_id_data_s(char *str, size_t size, struct SListNodeData *data);
-char *get_id_dest_data_s(struct SListNodeData *data);
-int get_val_data_s(struct SListNodeData *data);
-struct SListNodeData *get_data_s(struct SListNode *node);
-void set_data_node_s(struct SListNode *node, struct SListNodeData *data);
-void set_id_data_s(struct SListNodeData *data, const char *id);
-void set_id_dest_data_s(struct SListNodeData *data, char *id_dest);
-void set_val_data_s(struct SListNodeData *data, int val);
+extern char *get_id_data_s(char *str, size_t size, struct SListNodeData *data);
+extern char *get_id_dest_data_s(struct SListNodeData *data);
+extern int get_val_data_s(struct SListNodeData *data);
+extern struct SListNodeData *get_data_s(struct SListNode *node);
+extern void set_data_node_s(struct SListNode *node, struct SListNodeData *data);
+extern void set_id_data_s(struct SListNodeData *data, const char *id);
+extern void set_id_dest_data_s(struct SListNodeData *data, char *id_dest);
+extern void set_val_data_s(struct SListNodeData *data, int val);
 
 /// Next nodes
-struct SListNode *get_next_node_s(struct SListNode *node);
-void set_next_node_s(struct SListNode *node, struct SListNode *next);
+extern struct SListNode *get_next_node_s(struct SListNode *node);
+extern void set_next_node_s(struct SListNode *node, struct SListNode *next);
 
 /// Remove nodes
-struct SListNode *remove_node_s(struct SList *list, struct SListNode *target);
+extern struct SListNode *remove_node_s(struct SList *list,
+                                       struct SListNode *target);
 
 /// Append and prepend nodes
-void append_node_s(struct SList *list, struct SListNode *anchor,
-                   struct SListNode *new_node);
-void prepend_node_s(struct SList *list, struct SListNode *anchor,
-                    struct SListNode *new_node);
+extern void append_node_s(struct SList *list, struct SListNode *anchor,
+                          struct SListNode *new_node);
+extern void prepend_node_s(struct SList *list, struct SListNode *anchor,
+                           struct SListNode *new_node);
 
 /// Head nodes
-struct SListNode *get_head_list_s(struct SList *list);
-void set_head_list_s(struct SList *list, struct SListNode *head);
+extern struct SListNode *get_head_list_s(struct SList *list);
+extern void set_head_list_s(struct SList *list, struct SListNode *head);
 
 //// Push and pop head nodes
-struct SListNode *pop_head_node_s(struct SList *list);
-void push_head_node_s(struct SList *list, struct SListNode *new_node);
+extern struct SListNode *pop_head_node_s(struct SList *list);
+extern void push_head_node_s(struct SList *list, struct SListNode *new_node);
 
 /// Middle nodes
-struct SListNode *get_mid_list_s(struct SList *list);
-void set_mid_list_s(struct SList *list, struct SListNode *mid);
+extern struct SListNode *get_mid_list_s(struct SList *list);
+extern void set_mid_list_s(struct SList *list, struct SListNode *mid);
 
 //// Push and pop middle nodes
-struct SListNode *pop_mid_node_s(struct SList *list);
-void push_mid_node_s(struct SList *list, struct SListNode *new_node);
+extern struct SListNode *pop_mid_node_s(struct SList *list);
+extern void push_mid_node_s(struct SList *list, struct SListNode *new_node);
 
 //// Recalculate middle-nodes
-void recalc_mid_list_s(struct SList *list);
+extern void recalc_mid_list_s(struct SList *list);
 
 /// Tail nodes
-struct SListNode *get_tail_list_s(struct SList *list);
-void set_tail_list_s(struct SList *list, struct SListNode *tail);
+extern struct SListNode *get_tail_list_s(struct SList *list);
+extern void set_tail_list_s(struct SList *list, struct SListNode *tail);
 
 //// Push and pop tail nodes
-struct SListNode *pop_tail_node_s(struct SList *list);
-void push_tail_node_s(struct SList *list, struct SListNode *new_node);
+extern struct SListNode *pop_tail_node_s(struct SList *list);
+extern void push_tail_node_s(struct SList *list, struct SListNode *new_node);
 
 // List length
-size_t get_len_list_s(struct SList *list);
-void recalc_len_list_s(struct SList *list);
-void set_len_list_s(struct SList *list, size_t len);
+extern size_t get_len_list_s(struct SList *list);
+extern void recalc_len_list_s(struct SList *list);
+extern void set_len_list_s(struct SList *list, size_t len);
 
 // Print list nodes and lists
-void print_list_s(struct SList *list);
-void print_node_s(struct SListNode *node);
+extern void print_list_s(struct SList *list);
+extern void print_node_s(struct SListNode *node);
 
 #endif
